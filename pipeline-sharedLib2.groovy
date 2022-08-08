fd238e17115336f4c530046dca338295dc8f0dde
omer-demirozu <omer.demirozu@vodafone.com>
2022-08-08 17:24:30 +0300

    sharedLib pipeline2 added

--- pipeline-sharedLib2.groovy
+++ pipeline-sharedLib2.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Example') {
             steps {
-                helloWorld()
+                helloWorld(name:Omer, dayofWeek:Tuesday)
             }
         }
     }
